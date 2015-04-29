f18ddc6e1481a85879ff71f12ef4fceb0120d89a
Philip Rudy <me@philiparudy.com>
2015-04-28 23:30:59 -0400

    Static Homepage

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:200,500,700,400,300' rel='stylesheet' type='text/css'>
+  
+    <link rel="stylesheet" type="text/css" href="stylesheets/app.css">
+
+   
+</head>
+<body>
+	<div class="off-canvas-wrap" data-offcanvas>
+		<div class="inner-wrap">
+			
+			
+			
+			<aside class="right-off-canvas-menu canvas">
+						<ul class="off-canvas-list text-center">
+											<li><label>Main Menu</label></li>
+											<li><a class="main-link" href="">Home</a></li>
+											<li><a class="main-link" href="">Gallery</a>
+												<ul class="submenu">
+													<li><a href="">Invitation Studio</a></li>
+													<li><a href="">Design Studio</a></li>
+												</ul>
+											</li>
+											<li><a class="main-link" href="">Info</a>
+												<ul class="submenu">
+													<li><a href="">Process</a></li>
+													<li><a href="">Investment</a></li>
+													<li><a href="">FAQ</a></li>
+													<li><a href="">Invitation Wording</a></li>
+													<li><a href="">Pros We Love!</a></li>
+												</ul>
+											</li>
+											<li><a class="main-link" href="">About</a>
+													<ul class="submenu">
+														<li><a href="">About April</a></li>
+														<li><a href="">Press</a></li>
+														<li><a href="">Reviews</a></li>
+													</ul>
+											</li>
+											<li><a class="main-link" href="">Contact</a></li>
+											<li><a class="main-link" href="">Blog</a></li>
+											<li><a class="main-link" href="">Shop</a></li>
+						</ul>
+						<h4 style="width: 60%; display: table; margin: 20px auto;" class="h4 ribbon"><span class="left-ribbon"></span><a class="right-off-canvas-toggle">close menu</a><span class="right-ribbon"></span></h4>
+			</aside>
+	<div class="home page-wrapper">
+		
+		<header>
+			<div class="row">
+				<div class="small-18 small-centered medium-uncentered medium-5 large-4 columns">
+					<div id="logo-wrapper">
+						<a class="hide-for-small-only" href="/"><img src="images/logo.png" "April Lynn Designs" /></a>
+						<a class="mobile-logo show-for-small-only" href="/"><img src="images/mobile-logo.png" "April Lynn Designs" /></a>
+					</div>
+					<div class="show-for-small-only small-17 medium-6 small-centered columns">
+						<ul class="small-block-grid-6">
+							<li><a href=""><img src="images/facebook.png" alt="facebook" /></a></li>
+							<li><a href=""><img src="images/twitter.png" alt="twitter" /></a></li>
+							<li><a href=""><img src="images/pinterest.png" alt="pinterest" /></a></li>
+							<li><a href=""><img src="images/instagram.png" alt="instagram" /></a></li>
+							<li><a href=""><img src="images/rss.png" alt="rss" /></a></li>
+							<li><a href=""><img src="images/gplus.png" alt="gplus" /></a></li>
+						</ul>
+					</div>
+					<div class="mobile-menu show-for-small-only">
+						<h4 class="h4 ribbon"><span class="left-ribbon"></span><a href="#" class="right-off-canvas-toggle">main menu</a><span class="right-ribbon"></span></h4>
+					</div>
+					<section id="left-nav">
+						
+						<div class="hide-for-small-only circle-wrap">
+							<div class="circle">
+							</div>
+						</div>
+						<div class="hide-for-small-only aboutapril">
+							<p>This would just be a small paragraph like you were talking about</p>
+						</div>
+						<hr />
+						<div class="left-nav-wrap hide-for-small-only">
+							<div class="secondary-nav">
+								<h4 class="h4 ribbon"><span class="left-ribbon"></span>stationary<span class="right-ribbon"></span></h4>
+								<ul>
+									<li><a href="">Wedding Designs</a></li>
+									<li><a href="">Save the Dates</a></li>
+									<li><a href="">Bridal Shower</a></li>
+									<li><a href="">Birth Announcement</a></li>
+									<li><a href="">Birthday</a></li>
+									<li><a href="">Engagement</a></li>
+									<li><a href="">Wedding Day</a></li>
+								</ul>
+							</div>
+							<div class="secondary-nav">
+								<h4 class="h4 ribbon"><span class="left-ribbon"></span>design<span class="right-ribbon"></span></h4>
+								<ul>
+									<li><a href="">Business Cards</a></li>
+									<li><a href="">Flyers</a></li>
+									<li><a href="">Logos</a></li>
+									<li><a href="">Promortional</a></li>
+									<li><a href="">Social Media</a></li>
+								</ul>
+							</div>
+							<div class="secondary-nav">
+								<h4 class="h4 ribbon"><span class="left-ribbon"></span>information<span class="right-ribbon"></span></h4>
+								<ul>
+									<li><a href="">Process</a></li>
+									<li><a href="">Investment</a></li>
+									<li><a href="">FAQ</a></li>
+									<li><a href="">Invitation Wedding</a></li>
+									<li><a href="">Pros We Love</a></li>
+								</ul>
+							</div>
+						</div>
+					</section>
+				</div>
+				<div class="small-18 small-centered medium-uncentered medium-13  large-14 columns">
+					<nav class="hide-for-small-only ribbon">
+						<ul class="">
+											<li><a class="main-link" href="">Home</a></li>
+											<li><a class="main-link" href="">Gallery</a>
+												<ul class="submenu">
+													<li><a href="">Invitation Studio</a></li>
+													<li><a href="">Design Studio</a></li>
+												</ul>
+											</li>
+											<li><a class="main-link" href="">Info</a>
+												<ul class="submenu">
+													<li><a href="">Process</a></li>
+													<li><a href="">Investment</a></li>
+													<li><a href="">FAQ</a></li>
+													<li><a href="">Invitation Wording</a></li>
+													<li><a href="">Pros We Love!</a></li>
+												</ul>
+											</li>
+											<li><a class="main-link" href="">About</a>
+													<ul class="submenu">
+														<li><a href="">About April</a></li>
+														<li><a href="">Press</a></li>
+														<li><a href="">Reviews</a></li>
+													</ul>
+											</li>
+											<li><a class="main-link" href="">Contact</a></li>
+											<li><a class="main-link" href="">Blog</a></li>
+											<li><a class="main-link" href="">Shop</a></li>
+						</ul>
+						<span class="right-ribbon"></span>
+					</nav>
+					<div class="home-banner">
+						<img src="images/banner.png" />
+					</div>
+					<div class="home-content">
+						<h3>custom stationary & design studio</h3>
+						
+						<p>April Lynn Designs (formerly ALH Designs) is an award-winning custom stationery + design studio located in the suburbs of Philadelphia, Pennsylvania. We specialize in unique custom wedding stationery and paper goods for all of the important social events in your life (engagements, anniversaries, birthdays, mitzvahs, bridal showers, baby showers, birth announcements, baptisms, graduations, and holidays). Our work has been featured on Elizabeth Anne Designs, Every Last Detail, Heart Love Always, Lover.ly and WeddingLovely as well as other wedding planning sites.</p>
+	
+						<p>Design services for coordinating pieces are offered, so that all of your wedding stationery has a cohesive look (save the dates, wedding programs, menus, table numbers, escort cards, place cards, seating charts, mad libs guestbook cards and thank you cards). We offer digital, offset, thermography (raised printing), foil stamping, letterpress and engraving printing techniques on a large selection of luxurious and unique paper.</p>
+						
+						<p>Contact us today for your complimentary consultation.</p>
+						<hr />
+						<h3 class="ribbon"><span>recent blog posts</span></h3>
+							<div class="blog-post-wrapper">
+								<div class="row">
+									<div class="small-18 medium-8 blog-columns">
+										<div class="meta-wrapper">
+											<div class="blog-meta">
+												<h4 class="primary blog-title isabella">This is an Example Blog Title</h4>
+												<p>This is an example excerpt for a blog post for which we will enter.</p>
+												<a class="h4 ribbon read-more"><span class="left-ribbon"></span>Read More<span class="right-ribbon"></span></a>
+											</div>
+										</div>
+										<img src="images/banner.png" />
+									</div>
+									<div class="small-18 medium-8 blog-columns">
+										<div class="meta-wrapper">
+											<div class="blog-meta">
+												<h4 class="primary blog-title isabella">This is an Example Blog Title</h4>
+												<p>This is an example excerpt for a blog post for which we will enter.</p>
+												<a class="h4 ribbon read-more"><span class="left-ribbon"></span>Read More<span class="right-ribbon"></span></a>
+											</div>
+										</div>
+										<img src="images/banner.png" />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		</header>
+		
+		
+		<section id="main-body">
+			<div class="row">
+				<div class="small-3 columns">
+				</div>
+				<div class="small-9 columns">
+					
+				</div>
+			</div>
+		</section>
+		<hr />
+		<div class="row social-media">
+			<div class="small-12 medium-6 small-centered columns">
+				<ul class="small-block-grid-6">
+					<li><a href=""><img src="images/facebook.png" alt="facebook" /></a></li>
+					<li><a href=""><img src="images/twitter.png" alt="twitter" /></a></li>
+					<li><a href=""><img src="images/pinterest.png" alt="pinterest" /></a></li>
+					<li><a href=""><img src="images/instagram.png" alt="instagram" /></a></li>
+					<li><a href=""><img src="images/rss.png" alt="rss" /></a></li>
+					<li><a href=""><img src="images/gplus.png" alt="gplus" /></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer-wrap text-center">
+			<p>APRIL LYNN DESIGNS (FORMERLY ALH DESIGNS) | CUSTOM WEDDING INVITATIONS AND EVENT STATIONARY | 267-888-6095 | 2010-201 APRIL LUNN DESIGNS - ALL RIGHTS RESERVED<br />
+			106 SOUTH BELLEVUE AVENUE | SUITE 204 | LANGHORN, PENNSYLVANIA 19047 | HOURS BY APPOINTMENT ONLY<br />
+			SITE BY LALU DESIGNS
+		</div>
+	</div>
+		<footer>
+		</footer>
+  <!--<a href="#" class="button" data-reveal-id="myNormal">click</a>
+  <div class="reveal-modal" id="myNormal">
+      Modal
+      <a class="close-reveal-modal">x</a>
+  </div>-->
+   <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+   <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+   <script src="js/foundation.js"></script>
+   <script src="js/foundation.offcanvas.js"></script>
+   <script src="js/foundation.reveal.js"></script>
+   
+   <script>
+     $(document).foundation({
+		  offcanvas : {
+		    // Sets method in which offcanvas opens.
+		    // [ move | overlap_single | overlap ]
+		    open_method: 'overlap', 
+		    // Should the menu close when a menu link is clicked?
+		    // [ true | false ]
+		    close_on_click : false
+		  }
+	});
+   </script>
+		</div>
+	</div>
+	<a class="exit-off-canvas">
+</body>
